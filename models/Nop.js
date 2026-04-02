a79d6130493144710188d320ec279fabb905dad5
Devrock <devbhakat05@gmail.com>
2026-04-02 05:30:36 +0530

    Delete models directory

--- models/Nop.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const nopSchema = new mongoose.Schema({
-    userId: { type: String, required: true, unique: true },
-    expiresAt: { type: Date, default: null }
-});
-
-module.exports = mongoose.model('Nop', nopSchema);
-
